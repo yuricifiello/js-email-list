d71b672a19d00d6729053177e6ef48030bac1cd5
yuricifiello <yuricifiello@gmail.com>
2025-04-23 17:35:26 +0200

    add axios

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const emailList = document.getElementById("email-list");
 
 for (let i = 0; i < 10; i++) {}
+axios
+  .get("https://flynn.boolean.careers/exercises/api/random/mail")
+  .then((response) => {});
