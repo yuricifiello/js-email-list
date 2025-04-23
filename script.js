3bde31c698128b667c2fa5798b58e2d2f6402d25
yuricifiello <yuricifiello@gmail.com>
2025-04-23 17:37:09 +0200

    create list item

--- script.js
+++ script.js
@@ -5,4 +5,5 @@ axios
   .get("https://flynn.boolean.careers/exercises/api/random/mail")
   .then((response) => {
     const email = response.data.response;
+    const li = document.createElement("li");
   });
